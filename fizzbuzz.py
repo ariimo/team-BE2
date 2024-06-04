c907cd622d2820484a41e8759e05e00600d9b751
박민규 <pmq0101@naver.com>
2024-06-04 20:28:30 +0900

    refactor: 오류 수정

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,7 +3,7 @@ for i in range(1, 15 + 1):
     if i % 3 == 0 and i % 5 == 0:
         print("fizz buzz")
     # 5로 나누었을 때 구현 - 아림
-    elif: i % 5 == 0:
+    elif i % 5 == 0:
         print('buzz')
     # 3로 나누었을 때 구현 - 인제
     elif i % 3 == 0:
