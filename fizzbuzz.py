3052fb7aa7e065cfe8821e196b178f7104119a68
박민규 <pmq0101@naver.com>
2024-06-04 20:01:03 +0900

    feat: 기본 기능 구현

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+for i in range(1, 15 + 1):
+    # 15로 나누었을 때 구현 - 홍식
+    if:
+        print()
+    # 5로 나누었을 때 구현 - 아림
+    elif:
+        print()
+    # 3로 나누었을 때 구현 - 인제
+    elif:
+        print()
+    else:
+        print(i)
