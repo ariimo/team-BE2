2913d504af99d86806133cd402bbb41d66309a0e
박민규 <pmq0101@naver.com>
2024-06-04 20:43:51 +0900

    feat: 역할 분담 코드

--- MontyHallProblem.py
+++ MontyHallProblem.py
@@ -1,2 +1,13 @@
 import random
 user_input = int(input("몇 번 실행할 건지 입력하세요 : "))
+
+win = 0
+for i in range(user_input):
+    prize = random.randint(1, 3)
+    choice = 1
+    # 홍식
+    if prize == choice:
+    # 아림
+    elif prize == 2:
+    # 인제
+    else:
